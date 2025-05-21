6389a05a40f1bd70a31820dca9236ffda6702f28
gmena <menagianc@gmail.com>
2025-05-21 12:07:05 -0400

    2nd Commit!

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/labdelmar/', // Add this line with your repository name
   plugins: [react()],
 })
